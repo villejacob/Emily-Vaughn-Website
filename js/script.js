16d691da48785205041b6a54ea42525fd078b636
Jacob Ville <VilleLJacob@gmail.com>
2016-12-30 16:59:01 -0600

    Fix video play on load, add name logo and initial transitions

--- js/script.js
+++ js/script.js
@@ -2,6 +2,15 @@
 //    $(this).fadeOut()
 // })
 
-// $('video').bind('ended', function(){
-// 	$(this).animate({ opacity: 0 }, 1800); 
-// });
+$('video').bind('ended', function(){
+	$(this).fadeOut(1800);
+});
+
+// Fix iOS bugs
+var iOS = /iPad|iPhone|iPod/.test(navigator.platform);
+if( iOS ) {
+    var background_videos = document.querySelectorAll('.video-bg video');
+    for( i=0; i<background_videos.length; i++ ) {
+        background_videos[i].parentNode.removeChild(background_videos[i]);
+    }
+}
