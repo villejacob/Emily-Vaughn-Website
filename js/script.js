0d2df8d248542e416ab0a5a2abe33af876691b01
Jacob Ville <VilleLJacob@gmail.com>
2017-01-08 18:20:40 -0500

    Restructure page positioning to improve animations

--- js/script.js
+++ js/script.js
@@ -1,20 +1,23 @@
-var video = $('video')[0];
 
-// Attach a "timeupdate" event to the video
-video.addEventListener("timeupdate", getCurrentTime);
+$('.landing-inner').css('visibility', 'visible');
 
-// Display title once video slows
-function getCurrentTime() { 
-	if (video.currentTime > 2.2){
-		$('#emily-name').css('visibility', 'visible');
-	};
-};
+// var video = $('video')[0];
 
-// Fade to pink once video is finished
-$('video').bind('ended', function(){
-	$(this).fadeOut(1800);
-	$('.landing-inner a').css('visibility', 'visible');
-});
+// // Attach a "timeupdate" event to the video
+// video.addEventListener("timeupdate", getCurrentTime);
+
+// // Display title once video slows
+// function getCurrentTime() { 
+// 	if (video.currentTime > 2.2){
+// 		$('#emily-name').css('visibility', 'visible');
+// 	};
+// };
+
+// // Fade to pink once video is finished
+// $('video').bind('ended', function(){
+// 	$(this).fadeOut(1800);
+// 	$('.landing-inner a').css('visibility', 'visible');
+// });
 
 // Fix iOS bugs
 var iOS = /iPad|iPhone|iPod/.test(navigator.platform);
