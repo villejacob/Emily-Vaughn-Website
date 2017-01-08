156bef0882eb1a19807d7cc36ab64aa17c931cf7
Jacob Ville <VilleLJacob@gmail.com>
2017-01-08 18:34:49 -0500

    Correct hover animations

--- js/script.js
+++ js/script.js
@@ -1,23 +1,20 @@
+var video = $('video')[0];
 
-$('.landing-inner').css('visibility', 'visible');
+// Attach a "timeupdate" event to the video
+video.addEventListener("timeupdate", getCurrentTime);
 
-// var video = $('video')[0];
+// Display title once video slows
+function getCurrentTime() { 
+	if (video.currentTime > 2.2){
+		$('#emily-name').css('visibility', 'visible');
+	};
+};
 
-// // Attach a "timeupdate" event to the video
-// video.addEventListener("timeupdate", getCurrentTime);
-
-// // Display title once video slows
-// function getCurrentTime() { 
-// 	if (video.currentTime > 2.2){
-// 		$('#emily-name').css('visibility', 'visible');
-// 	};
-// };
-
-// // Fade to pink once video is finished
-// $('video').bind('ended', function(){
-// 	$(this).fadeOut(1800);
-// 	$('.landing-inner a').css('visibility', 'visible');
-// });
+// Fade to pink once video is finished
+$('video').bind('ended', function(){
+	$(this).fadeOut(1800);
+	$('.landing-inner a').css('visibility', 'visible');
+});
 
 // Fix iOS bugs
 var iOS = /iPad|iPhone|iPod/.test(navigator.platform);
