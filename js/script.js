8a2eaacd47e022ad920bac1f503b743c36619d3b
Jacob Ville <VilleLJacob@gmail.com>
2017-03-06 18:52:01 -0500

    Add new intro video, format video and name sizing

--- js/script.js
+++ js/script.js
@@ -1,26 +1,12 @@
-var video = $('video')[0];
-
-// Attach a "timeupdate" event to the video
-video.addEventListener("timeupdate", getCurrentTime);
-
-// Transition based on the playback time
-function getCurrentTime() { 
-	// Display Emily's name at the dope slow motion bat swing
-	if (video.currentTime > 2.2){ $('#emily-name').css('visibility', 'visible'); }
-	// Fade to pink before reaching end of video
-	if (video.currentTime > 4.8){ $('video').css('opacity', '0'); }
-};
-
 // Animate text once video ends
 $('video').bind('ended', function(){
-	$('#emily-name').css('top', '15.4vh');
-	$('.landing-inner')
-		.delay(2400)
-		.queue(function(next){
-			$(this).css('visibility', 'visible');
-			next();
-		});
-	// $('a#spotify').css('margin', 'auto 40.7vw');
+	function showPage() {
+		$('#emily-name').css('top', '15.4vh');
+		$('.landing-inner').css('visibility', 'visible');
+	}
+	$('video').css('opacity', '0');	
+	$('#emily-name').css('visibility', 'visible')	
+	setTimeout(showPage, 1600);
 });
 
 // Fix iOS bugs
